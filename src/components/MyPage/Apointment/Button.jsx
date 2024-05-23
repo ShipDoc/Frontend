b32dc04cca11abca2e68dffbba4e61e3c96323be
[nyun-nye] <[2001_11_7@naver.com]>
2024-05-23 22:06:51 +0900

    MyPage Button 수정

--- src/components/MyPage/Apointment/Button.jsx
+++ src/components/MyPage/Apointment/Button.jsx
@@ -1,50 +1,85 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ButtonItem from '../ButtonItem';
 import { useNavigate } from "react-router-dom";
 
 const Button = () => {
   const navigate = useNavigate();
-  const [name, setName] = useState("김아현");
-  const [hospital, setHospital] = useState("연세이빈후과의원");
-  const [date, setDate] = useState("2024. 05. 07 화요일 10:00");
-  const [min, setMin] = useState("10");
+  
+  const [buttonData, setButtonData] = useState([
+    {
+      name: "김아현",
+      hospital: "연세이빈후과의원",
+      date: "2024. 05. 07 화요일 10:00",
+      min: "10"
+    },
+    {
+      name: "김레오",
+      hospital: "연세이빈후과의원",
+      date: "2024. 05. 07 화요일 10:00",
+      min: "10"
+    },
+  ]);
 
-  const handleButton1 = () => {
+  const handleButtonClick = () => {
     navigate("detail");
   };
 
+
+  // *****나중에 추가 삭제...
+  const handleAddButton = () => {
+    const newButton = {
+      name: "새로운 이름",
+      hospital: "새로운 병원",
+      date: "새로운 날짜",
+      min: "새로운 대기시간"
+    };
+    setButtonData([...buttonData, newButton]);
+  };
+
+  const handleDeleteButton = (index) => {
+    const newData = buttonData.filter((_, i) => i !== index);
+    setButtonData(newData);
+  };
+
   return (
-    <ButtonContainer>
-      <ClickableButtonItem onClick={handleButton1}>
-        <ButtonItem
-          name={name}
-          hospital={hospital}
-          date={date}
-          ment="예상 대기시간"
-          min={`${min}분`}
-        />
-        <ButtonItem
-          name={name}
-          hospital={hospital}
-          date={date}
-          ment="예상 대기시간"
-          min={`${min}분`}
-        />
-      </ClickableButtonItem>
-      
-    </ButtonContainer>
+    <div>
+      <ButtonContainer>
+        {buttonData.map((button, index) => (
+          <ClickableButtonItem key={index} onClick={handleButtonClick}>
+            <ButtonItem
+              name={button.name}
+              hospital={button.hospital}
+              date={button.date}
+              ment="예상 대기시간"
+              min={`${button.min}분`}
+            />
+          </ClickableButtonItem>
+        ))}
+      </ButtonContainer>
+    </div>
   );
 };
 
 const ButtonContainer = styled.div`
   font-family: Pretendard;
-  align-item: center;
+  align-items: center;
   margin-bottom: 10rem;
 `;
 
 const ClickableButtonItem = styled.div`
   cursor: pointer;
+  position: relative;
+`;
+
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
+const AddButton = styled.button`
+  margin-top: 20px;
 `;
 
 export default Button;
